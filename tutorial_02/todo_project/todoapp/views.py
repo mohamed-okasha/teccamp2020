892259e77ed349a6825777039fc4853b87f70e81
K <anastaleb44@gmail.com>
2020-05-07 15:35:28 +0200

    Anas

--- tutorial_02/todo_project/todoapp/views.py
+++ tutorial_02/todo_project/todoapp/views.py
@@ -5,23 +5,28 @@ from .models import Category, Todo
 def index(request):
     
     if request.method == 'POST':
+        if 'save' in request.POST:
+            title   =  request.POST['title']
+            content   =  request.POST['content']
+            due_date   =  request.POST['due_date']
+            category_id = request.POST['selected_category']
+            category = Category.objects.get(id=category_id)
+            todo = Todo(title=title, content=content, due_date=due_date,category=category)
+            todo.save()
+
+            print('New Task Title:'+ title)
+            print('New Task Content:'+ content)
+            print('New Task date:'+ due_date)
+            print('New Task category_id:'+ category.name)
+
+
+            print(request.POST)
+        if 'delete' in request.POST:
+            todo_id= request.POST['val']
+            dele=Todo.objects.get(id=todo_id)
+            dele.delete()
+
         
-        title   =  request.POST['title']
-        content   =  request.POST['content']
-        due_date   =  request.POST['due_date']
-        category_id = request.POST['selected_category']
-
-        print(request.POST)
-
-        category = Category.objects.get(id=category_id)
-        todo = Todo(title=title, content=content, due_date=due_date,category=category)
-        todo.save()
-
-        print('New Task Title:'+ title)
-        print('New Task Content:'+ content)
-        print('New Task date:'+ due_date)
-        print('New Task category_id:'+ category.name)
-
         # redirect('/todos')
         
 
